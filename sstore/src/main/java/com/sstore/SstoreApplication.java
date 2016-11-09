874f6f2c392626b7ba5b6a2f96356e5108c74071
nku103 <nku103@NOINKU10331291.sapient.com>
2016-11-09 16:19:56 +0530

    Created

---
+++ sstore/src/main/java/com/sstore/SstoreApplication.java
@@ -0,0 +1,12 @@
+package com.sstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SstoreApplication.class, args);
+	}
+}
