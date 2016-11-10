33cfd6cc16c4bc60de17f0dfd69fba6cda8e3030
nku103 <nku103@NOINKU10331291.sapient.com>
2016-11-10 11:41:42 +0530

    a1

--- sstore/src/main/java/com/sstore/SstoreApplication.java
+++ sstore/src/main/java/com/sstore/SstoreApplication.java
@@ -2,8 +2,10 @@ package com.sstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.sstore", "com.sstore.controller", "com.sstore.config"})
 public class SstoreApplication {
 
 	public static void main(String[] args) {
